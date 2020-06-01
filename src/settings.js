e9461faec470614671081c1b3a011e8e5def3757
Şakir Mehmetoğlu (SuStartX) <sakir.mehmetoglu@gmail.com>
2020-06-01 21:19:00 +0300

    Ayarların veitabanından çekilmesi için gerekli kodlar yazıldı.

---
+++ src/settings.js
@@ -0,0 +1,15 @@
+import { dbSelect } from './libs/DB'
+
+async function getSettings () {
+  var settings = {}
+  dbSelect('select * from settings', 'all', function (err, result) {
+    if (err === null && typeof result !== 'undefined') {
+      result.forEach(item => {
+        settings[item.key] = item.value
+      })
+    }
+  })
+  return settings
+}
+
+export { getSettings }
