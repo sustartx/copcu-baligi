4d458c422dc9b6e73eb2bfabd749f85153663805
Şakir Mehmetoğlu (SuStartX) <sakir.mehmetoglu@gmail.com>
2020-06-02 14:34:45 +0300

    Menü harici bir dosyaya taşındı.

--- src/App.vue
+++ src/App.vue
@@ -3,47 +3,7 @@
       <a id="show-sidebar" class="btn btn-sm btn-dark" href="#">
           <i class="fas fa-bars"></i>
       </a>
-      <nav id="sidebar" class="sidebar-wrapper">
-          <div class="sidebar-content">
-              <div class="sidebar-brand">
-                  <a href="#">Çöpçü Balığı</a>
-                  <div id="close-sidebar">
-                      <i class="fas fa-times"></i>
-                  </div>
-              </div>
-              <div class="sidebar-search">
-                  <div>
-                      <div class="input-group">
-                          <input type="text" class="form-control search-menu" placeholder="Ara...">
-                          <div class="input-group-append">
-                            <span class="input-group-text">
-                              <i class="fa fa-search" aria-hidden="true"></i>
-                            </span>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="sidebar-menu">
-                  <ul>
-                      <li class="header-menu">
-                          <span>General</span>
-                      </li>
-                      <li>
-                          <router-link to="/">
-                              <i class="fa fa-tachometer-alt"></i>
-                              Anasayfa
-                          </router-link>
-                      </li>
-                      <li>
-                          <router-link to="/settings">
-                            <i class="fa fa-cogs"></i>
-                            Ayarlar
-                          </router-link>
-                      </li>
-                  </ul>
-              </div>
-          </div>
-      </nav>
+      <sidebar></sidebar>
       <main class="page-content">
           <div class="container-fluid">
               <router-view/>
@@ -53,8 +13,11 @@
 </template>
 
 <script>
+import Sidebar from './views/layout/Sidebar'
 export default {
-
+  components: {
+    Sidebar
+  }
 }
 </script>
 
---
+++ src/views/layout/Sidebar.vue
@@ -0,0 +1,63 @@
+<template>
+  <nav id="sidebar" class="sidebar-wrapper">
+    <div class="sidebar-content">
+      <div class="sidebar-brand">
+        <a href="#">Çöpçü Balığı</a>
+        <div id="close-sidebar">
+          <i class="fas fa-times"></i>
+        </div>
+      </div>
+      <div class="sidebar-search">
+        <div>
+          <div class="input-group">
+            <input type="text" class="form-control search-menu" placeholder="Ara...">
+            <div class="input-group-append">
+                <span class="input-group-text">
+                  <i class="fa fa-search" aria-hidden="true"></i>
+                </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="sidebar-menu">
+        <ul>
+          <li class="header-menu">
+            <span>General</span>
+          </li>
+          <li v-for="(item, key) in menu">
+            <router-link :to="item.link" :key="key">
+              <i :class="item.class"></i>
+              {{ item.title }}
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  data () {
+    return {
+      menu: [
+        {
+          title: 'Dashboard',
+          class: 'fa fa-tachometer-alt',
+          link: '/'
+        },
+        {
+          title: 'Settings',
+          class: 'fa fa-cogs',
+          link: '/settings'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
